25beab594ed80a5f508f4c9218b5208a6b2f964c
EmeraldIngot <75592635+EmeraldIngot@users.noreply.github.com>
2025-04-18 16:28:20 -0400

    Force storage controller to always place facing up

--- src/main/java/com/emeraldingot/storagesystem/event/StorageControllerPlaceListener.java
+++ src/main/java/com/emeraldingot/storagesystem/event/StorageControllerPlaceListener.java
@@ -4,16 +4,23 @@ package com.emeraldingot.storagesystem.event;
 import com.emeraldingot.storagesystem.block.StorageControllerBlock;
 import com.emeraldingot.storagesystem.impl.ControllerManager;
 import com.emeraldingot.storagesystem.langauge.Language;
+import net.minecraft.world.level.block.BlockDispenser;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.block.Container;
 import org.bukkit.block.Dispenser;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.block.data.Directional;
+import org.bukkit.block.structure.StructureRotation;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.material.MaterialData;
+import org.bukkit.util.Vector;
 
 import javax.naming.ldap.Control;
 import java.sql.SQLException;
@@ -41,6 +48,11 @@ public class StorageControllerPlaceListener implements Listener {
         ControllerManager.getInstance().addController(block.getLocation());
 //        System.out.println(inventory.getClass());
 
+        BlockData blockData = event.getBlock().getBlockData();
+//        BlockFace blockFace = ((Directional) block.getBlockData()).getFacing();
+        ((Directional) blockData).setFacing(BlockFace.UP);
+        event.getBlock().setBlockData(blockData);
+//        System.out.println(blockFace.getDirection());
         event.setCancelled(false);
 
     }
