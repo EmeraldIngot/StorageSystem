57cbf575a5a05b008f49a069fa5e189ae1e4945f
EmeraldIngot <75592635+EmeraldIngot@users.noreply.github.com>
2025-07-26 21:04:47 -0400

    Update to 1.21.7+

---
+++ src/main/java/com/emeraldingot/storagesystem/gui/StorageSystemHolder.java
@@ -0,0 +1,11 @@
+package com.emeraldingot.storagesystem.gui;
+
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+
+public class StorageSystemHolder implements InventoryHolder {
+    @Override
+    public Inventory getInventory() {
+        return null;
+    }
+}
